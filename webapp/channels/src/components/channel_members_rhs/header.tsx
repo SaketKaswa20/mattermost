50544eaeca99256fb82f641e23a07b41ba02571d
Saket Kaswa <kaswasd21.mfg@coep.ac.in>
2023-10-29 09:33:29 +0530

    Replace usage of LocalizedIcon

--- webapp/channels/src/components/channel_members_rhs/header.tsx
+++ webapp/channels/src/components/channel_members_rhs/header.tsx
@@ -2,17 +2,17 @@
 // See LICENSE.txt for license information.
 
 import React from 'react';
-import {FormattedMessage} from 'react-intl';
+import { FormattedMessage, useIntl, injectIntl } from 'react-intl'; // Import useIntl and injectIntl
 import styled from 'styled-components';
 
-import type {Channel} from '@mattermost/types/channels';
+import type { Channel } from '@mattermost/types/channels';
 
-import LocalizedIcon from 'components/localized_icon';
 import OverlayTrigger from 'components/overlay_trigger';
 import Tooltip from 'components/tooltip';
 
 import Constants from 'utils/constants';
-import {t} from 'utils/i18n';
+
+// Remove import of LocalizedIcon
 
 interface Props {
     channel: Channel;
@@ -31,7 +31,9 @@ const HeaderTitle = styled.span`
     line-height: 2.4rem;
 `;
 
-const Header = ({channel, canGoBack, onClose, goBack}: Props) => {
+const Header = ({ channel, canGoBack, onClose, goBack }: Props) => {
+    const intl = useIntl(); // Use useIntl hook for localization
+
     const closeSidebarTooltip = (
         <Tooltip id='closeSidebarTooltip'>
             <FormattedMessage
@@ -44,35 +46,34 @@ const Header = ({channel, canGoBack, onClose, goBack}: Props) => {
     return (
         <div className='sidebar--right__header'>
             <span className='sidebar--right__title'>
-
                 {canGoBack && (
                     <BackButton
                         className='sidebar--right__back'
                         onClick={goBack}
                     >
-                        <i
-                            className='icon icon-arrow-back-ios'
-                            aria-label='Back Icon'
-                        />
+                        {channel.display_name && (
+                            <span
+                                className='icon icon-arrow-back-ios'
+                                aria-label={intl.formatMessage({ // Use formatMessage for localization
+                                    id: 'backIconAriaLabel',
+                                    defaultMessage: 'Back Icon',
+                                })}
+                            />
+                        )}
                     </BackButton>
                 )}
-
                 <HeaderTitle>
                     <FormattedMessage
                         id='channel_members_rhs.header.title'
                         defaultMessage='Members'
                     />
                 </HeaderTitle>
-
-                {channel.display_name &&
-                    <span
-                        className='style--none sidebar--right__title__subtitle'
-                    >
+                {channel.display_name && (
+                    <span className='style--none sidebar--right__title__subtitle'>
                         {channel.display_name}
                     </span>
-                }
+                )}
             </span>
-
             <OverlayTrigger
                 delayShow={Constants.OVERLAY_TIME_DELAY}
                 placement='top'
@@ -82,12 +83,14 @@ const Header = ({channel, canGoBack, onClose, goBack}: Props) => {
                     id='rhsCloseButton'
                     type='button'
                     className='sidebar--right__close btn btn-icon btn-sm'
-                    aria-label='Close'
+                    aria-label={intl.formatMessage({ // Use formatMessage for localization
+                        id: 'closeIconAriaLabel',
+                        defaultMessage: 'Close Sidebar Icon',
+                    })}
                     onClick={onClose}
                 >
-                    <LocalizedIcon
+                    <span
                         className='icon icon-close'
-                        ariaLabel={{id: t('rhs_header.closeTooltip.icon'), defaultMessage: 'Close Sidebar Icon'}}
                     />
                 </button>
             </OverlayTrigger>
